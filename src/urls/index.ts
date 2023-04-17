296f5705366512d5f8f582bc23bdedbcf19bdbd2
Mirco Veltri <github@mircoveltri.me>
2023-04-17 19:56:33 +0200

    refactor(urls): promote isUrl as package function. getBaseURL removed

--- src/urls/index.ts
+++ src/urls/index.ts
@@ -1,12 +1,23 @@
-import { DEV } from 'esm-env';
 import { isEmpty } from '../index.js';
 import { textBetween } from '../strings/index.js';
 import { ok, err, Ok, Err } from 'neverthrow';
 
-export function getBaseURL(devBase: string, prodBase: string): string {
-	return DEV ? devBase : prodBase;
+/**
+ * The function checks if a given string is a valid URL.
+ * @param {string} url - string parameter representing a URL that needs to be validated.
+ * @returns The function `isUrl` returns a boolean value (`true` or `false`) depending on whether the
+ * input `url` is a valid URL or not. If the input `url` can be successfully parsed as a URL using the
+ * `URL` constructor, the function returns `true`. Otherwise, if an error is thrown during parsing, the
+ * function returns `false`.
+ */
+export function isUrl(url: string): boolean {
+	try {
+		new URL(url);
+		return true;
+	} catch (err) {
+		return false;
+	}
 }
-
 /**
  * It takes a base URL and a pathname and returns a canonical URL.
  *
@@ -15,8 +26,78 @@ export function getBaseURL(devBase: string, prodBase: string): string {
  *
  * @returns A string representing the canonical URL.
  */
-export function canonicalURL(baseURL: string, pathname = ''): string {
-	return _trim(baseURL).concat(pathname);
+export function canonicalUrl(baseURL: string, pathname = ''): URL {
+	const urlString = _trim(baseURL).concat(pathname);
+	return new URL(urlString);
+}
+
+/**
+ * This function takes a URL, and returns a string that is the parent url
+ * without the last segment of the it.
+ *
+ * @param url - string - The base URL of the website.
+ *
+ * @returns A string representing the parent url of the given URL.
+ *
+ * @example
+ * ```
+ *  // Prints "https://example.com/blog/posts":
+ * parentUrl("https://example.com/blog/posts/welcome/").value
+ *
+ * ```
+ */
+export function parentUrl(url: string): Err<never, Error> | Ok<URL, never> {
+	let result = '';
+	if (!isUrl(url)) {
+		return err(new Error('Expected a valid URL'));
+	} else {
+		result = new URL(url).href.substring(0, new URL(url).href.lastIndexOf('/'));
+		return ok(new URL(result));
+	}
+}
+
+/**
+ * This function takes a URL, and returns a string that is the pathname
+ * without the last segment of the it.
+ *
+ * @param url - string - The base URL of the website.
+ *
+ * @returns A string representing the parent pathname of the given URL.
+ *
+ * @example
+ * ```
+ *  // Prints "blog/posts":
+ * parentPathname("https://example.com/blog/posts/welcome/").value
+ *
+ * ```
+ */
+export function parentPathname(url: string): Err<never, Error> | Ok<string, never> {
+	if (!isUrl(url)) {
+		return err(new Error('Expected a valid URL'));
+	} else {
+		const path = new URL(url).pathname.split('/');
+		const noEmptySegments = path.filter((s: string) => s !== '');
+
+		noEmptySegments.pop(); // remove the last
+		return ok(noEmptySegments.join('/'));
+	}
+}
+
+/**
+ * The function extracts the path segments from a valid URL and returns them as an array, or
+ * returns an error if the input is not a valid URL.
+ *
+ * @param {string} url - A string representing a URL.
+ *
+ * @returns An array of string segments extracted from the path of the input `url`.
+ */
+export function pathSegments(url: string): Err<never, Error> | Ok<string[], never> {
+	if (!isUrl(url)) {
+		return err(new Error('Expected a valid URL'));
+	} else {
+		const result = new URL(url).pathname.split('/').filter((part) => part?.trim() !== '');
+		return ok(result);
+	}
 }
 
 /**
@@ -80,57 +161,6 @@ export function makeImagePath(
 	return defaultImage(baseURL);
 }
 
-/**
- * This function takes a URL, and returns a string that is the parent url
- * without the last segment of the it.
- *
- * @param url - string - The base URL of the website.
- *
- * @returns A string representing the parent url of the given URL.
- *
- * @example
- * ```
- *  // Prints "https://example.com/blog/posts":
- * parentUrl("https://example.com/blog/posts/welcome/").value
- *
- * ```
- */
-export function parentUrl(url: string): Err<never, Error> | Ok<string, never> {
-	if (!_isUrl(url)) {
-		return err(new Error('Expected a valid URL'));
-	} else {
-		const result = new URL(url).href.substring(0, new URL(url).href.lastIndexOf('/'));
-		return ok(result);
-	}
-}
-
-/**
- * This function takes a URL, and returns a string that is the pathname
- * without the last segment of the it.
- *
- * @param url - string - The base URL of the website.
- *
- * @returns A string representing the parent pathname of the given URL.
- *
- * @example
- * ```
- *  // Prints "blog/posts":
- * parentPathname("https://example.com/blog/posts/welcome/").value
- *
- * ```
- */
-export function parentPathname(url: string): Err<never, Error> | Ok<string, never> {
-	if (!_isUrl(url)) {
-		return err(new Error('Expected a valid URL'));
-	} else {
-		const path = new URL(url).pathname.split('/');
-		const noEmptySegments = path.filter((s: string) => s !== '');
-
-		noEmptySegments.pop(); // remove the last
-		return ok(noEmptySegments.join('/'));
-	}
-}
-
 // --------------------------------------------------------------------------------------
 
 /**
@@ -151,13 +181,4 @@ function _trim(value: string): string {
 	let base = value.trim();
 	if (base === '/') base = '';
 	return base;
-}
-
-function _isUrl(url: string): boolean {
-	try {
-		new URL(url);
-		return true;
-	} catch (err) {
-		return false;
-	}
 }
--- tests/urls.test.ts
+++ tests/urls.test.ts
@@ -1,21 +1,42 @@
 import { describe, it, expect } from 'vitest';
 import { Ok, Err } from 'neverthrow';
-import { canonicalURL, makeImagePath, defaultImage, parentUrl, parentPathname } from '../src/urls';
+import {
+	isUrl,
+	canonicalUrl,
+	makeImagePath,
+	defaultImage,
+	defaultSocialImage,
+	parentUrl,
+	parentPathname,
+	pathSegments
+} from '../src/urls';
 
-describe('canonicalURL', () => {
+describe('urls', () => {
+	it('should be a valid URL', async () => {
+		const url = 'https://www.facebook.com/20531316728/posts/10154009990506729/';
+		expect(isUrl(url)).toBe(true);
+	});
+
+	it('should not be a valid url', async () => {
+		const url = '20531316728/posts/10154009990506729/';
+		expect(isUrl(url)).toBe(false);
+	});
+});
+
+describe('canonicalUrl', () => {
 	it('should be https://example.com/welcome', () => {
 		const url = 'https://example.com';
 		const path = '/welcome';
 
-		const result = canonicalURL(url, path);
-		expect(result).toBe('https://example.com/welcome');
+		const result = canonicalUrl(url, path);
+		expect(result.href).toBe('https://example.com/welcome');
 	});
 
 	it('should be origin only', () => {
 		const url = 'https://example.com';
 
-		const result = canonicalURL(url);
-		expect(result).toBe('https://example.com');
+		const result = canonicalUrl(url);
+		expect(result.href).toBe('https://example.com/');
 	});
 });
 
@@ -23,6 +44,23 @@ describe('defaultImage', () => {
 	it('should be a path to images/logs/logo.png', () => {
 		const imagePath = defaultImage('http://localhost:3000');
 		expect(imagePath).toBe('http://localhost:3000/images/logos/logo.png');
+	});
+});
+
+describe('defaultSocialImage', () => {
+	it('should be a path to images/socials.png', () => {
+		const imagePath = defaultSocialImage('http://localhost:3000');
+		expect(imagePath).toBe('http://localhost:3000/images/socials.png');
+	});
+
+	it('should be a path to assets/socials.png', () => {
+		const imagePath = defaultSocialImage('http://localhost:3000', 'assets');
+		expect(imagePath).toBe('http://localhost:3000/assets/socials.png');
+	});
+
+	it('should be a path to assets/github.png', () => {
+		const imagePath = defaultSocialImage('http://localhost:3000', 'assets', 'github.png');
+		expect(imagePath).toBe('http://localhost:3000/assets/github.png');
 	});
 });
 
@@ -46,19 +84,29 @@ describe('makeImagePath', () => {
 		const imagePath = makeImagePath('   ', 'resources', '/posts/welcome/', 'cover.png');
 		expect(imagePath).toBe('/resources/posts/welcome/cover.png');
 	});
+
+	it('should be a path to default image', () => {
+		const imagePath = makeImagePath('   ', 'resources', '/posts/welcome/', '');
+		expect(imagePath).toBe('/images/logos/logo.png');
+	});
+
+	it('should be a path to default image', () => {
+		const imagePath = makeImagePath('/', 'resources', '/posts/welcome/', '');
+		expect(imagePath).toBe('/images/logos/logo.png');
+	});
 });
 
 describe('parentUrl', () => {
 	it('should be https://example.com/blog/posts', () => {
 		const want = 'https://example.com/blog/posts';
-		const result = parentUrl('https://example.com/blog/posts/welcome') as Ok<string, never>;
-		expect(result.value).toBe(want);
+		const result = parentUrl('https://example.com/blog/posts/welcome') as Ok<URL, never>;
+		expect(result.value.href).toBe(want);
 	});
 
 	it('should be the url itself', () => {
-		const want = 'https://example.com';
-		const result = parentUrl('https://example.com') as Ok<string, never>;
-		expect(result.value).toBe(want);
+		const want = 'https://example.com/';
+		const result = parentUrl('https://example.com/') as Ok<URL, never>;
+		expect(result.value.href).toBe(want);
 	});
 
 	it('should be Error', () => {
@@ -95,4 +143,25 @@ describe('parentPathname', () => {
 		expect(result.error.message).toBe('Expected a valid URL');
 		expect(result.error).toStrictEqual(new Error('Expected a valid URL'));
 	});
+});
+
+describe('pathSegments', () => {
+	it('should return a string array', () => {
+		const t = 'https://example.com/blog/welcome';
+		const want = ['blog', 'welcome'];
+		const result = pathSegments(t) as Ok<string[], never>;
+		expect(result.value.length).toBe(want.length);
+		expect(result.value).toEqual(want);
+		expect(result.value[0]).toBe('blog');
+	});
+
+	it('should return Expected a valid URL as error message', () => {
+		const t = 'example.com/blog/welcome';
+		const result = pathSegments(t) as Err<never, Error>;
+		expect(result.error.message).toBe('Expected a valid URL');
+	});
+});
+
+describe('pathSegments', () => {
+	it('should return a string array', () => {});
 });
