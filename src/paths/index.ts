7b21c7f1116829058d8ecb5a36b5ade060898bb1
Mirco Veltri <github@mircoveltri.me>
2023-05-03 21:01:38 +0200

    refactor(paths): completed and TSDoc updated

--- src/paths/index.ts
+++ src/paths/index.ts
@@ -1,58 +1,291 @@
 /**
- * The function extracts the directory name from a given file path. It uses a regular expression to
- * match the last occurrence of a forward slash or backslash in the path, and returns everything
- * before that as the directory name. If there is no slash in the path, an empty string is returned.
+ * A bunch of utilities to deal with path strings.
  *
- * @param {string} thePath - The input parameter "thePath" is a string representing a file path.
+ * <ul>
+ * <li>The functions below do not interface with the real file system but they are used to extract some information from the given path string. It means not ensuring e.g. a file exists or a folder is a real folder on the disk. For that, simply use what already exists like {@link https://nodejs.org/api/path.html | Node's modules}.</li>
+ * <li>File and folder paths validation can be complex due to the variations in file path formats across different operating systems and file systems, e.g. both files and folders are valid even without extensions. </li>
+ * </ul>
  *
- * @returns the directory name of the given path.
+ * @packageDocumentation
  */
-export function dirname(thePath: string): string {
-	const result = thePath.match(/(.*)[\\/\\]/);
-	return result ? result[1] : '';
+
+import { Result, err, ok } from 'neverthrow';
+import { isDefined, isEmpty, isString } from '../is/index.js';
+
+/**
+ * Checks if a given path is a valid directory path.
+ *
+ * @param path - The path to be checked.
+ * @returns `true` if the path is a valid directory path, `false` otherwise.
+ *
+ * @example
+ * ```
+ * isDir('/home/demo/pictures')
+ * // => true
+ *
+ * isDir('/home/demo/pictures/dummy.jpeg')
+ * // => false
+ *
+ * isDir('C:\\Users\\Documents\\Folder1')
+ * // => true
+ *
+ * isDir('/var/www/html')
+ * // => true
+ *
+ * isDir('C:\\Users\\Documents\\File1.txt')
+ * // => false
+ * ```
+ */
+export function isDir(path: string): boolean {
+	if (!isString(path)) return false;
+
+	const unixRegex = /^(?!.*\/\0)[^\0]+(?<!\.\w+)$/;
+	const windowsRegex = /^[a-zA-Z]:\\(?:[^\\\0]+\\)*[^\\\0]+(?<!\.\w+)$/;
+
+	if (
+		(path.startsWith('/') && unixRegex.test(path)) ||
+		(path.match(/^[a-zA-Z]:\\/) && windowsRegex.test(path))
+	) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 /**
- * The function extracts the filename from a given file path string. It takes a string argument
- * `thePath` and returns the filename from the path by removing everything before the last forward slash or backslash character.
+ * Checks if a given path is a valid file path.
  *
- * @param {string} thePath - thePath is a string parameter that represents the file path from which
- * we want to extract the file name.
+ * @param path -The path to be checked.
+ * @returns `true` if the path is a valid file path, `false` otherwise.
  *
- * @returns a string value. The filename.
+ * @example
+ * ```
+ * isFile('/home/demo/pictures/dummy.jpeg')
+ * // => true
+ *
+ * isFile('/home/demo/pictures)
+ * // => true
+ *
+ * isFile('/home/demo/pictures/)
+ * // => false
+ *
+ * isFile('C:\\Users\\Documents\\File1.txt')
+ * // => true
+ *
+ * isFile('/var/www/html/index.html')
+ * // => true
+ *
+ * isFile('C:\\Users\\Documents\\Folder1')
+ * // => false
+ * ```
  */
-export function filename(thePath: string): string {
-	return thePath.replace(/^.*[\\\\/]/, '');
+export function isFile(path: string): boolean {
+	if (!isString(path)) return false;
+
+	const unixRegex = /^(?!.*\/\0)[^\0]+(\.[^\\/\0]+)?$/;
+	const windowsRegex = /^[a-zA-Z]:\\(?:\w+\\)*\w+\.\w+$/;
+
+	return (unixRegex.test(path) && !path.endsWith('/')) || windowsRegex.test(path);
 }
 
 /**
- * The function checks if a given string is a valid image file name with extensions such
+ * Extracts the directory name from a given file path.
+ *
+ * @param path - The file path from which to extract the directory name.
+ * @returns A `Result` object containing the directory name as a `string` on success,
+ * or an `Error` object if the provided input is not a string.
+ *
+ * @example
+ * ```
+ * dirname('/home/demo/pictures/dummy.jpeg').value
+ * // => "/home/demo/pictures"
+ *
+ * dirname('/home/demo/pictures').value
+ * // => "/home/demo/"
+ *
+ * dirname('C:/Users/Documents/File1.txt').value
+ * // => 'C:/Users/Documents'
+ *
+ * dirname('/var/www/html/index.html').value
+ * // => '/var/www/html'
+ *
+ * dirname('C:/Users/Documents/Folder1').value
+ * // => 'C:/Users/Documents'
+ *
+ * const dname = dirname(12345)
+ * dname.isOk()
+ * // => false
+ * dnane.isErr()
+ * // true
+ * dname.error.message
+ * // Outputs an error => '[paths.dirname] The given path must be a string'
+ * ```
+ */
+export function dirname(path: string): Result<string, Error> {
+	if (!isString(path)) {
+		return err(new Error('[paths.dirname] The given path must be a string'));
+	}
+
+	// Matches either backslash (\) or forward slash (/) as directory separator
+	const separator = /[\\\\/]/;
+	const parts = path.split(separator);
+	if (parts.length === 1) {
+		// one part only, the given path is just a file name
+		return ok(path);
+	}
+
+	// Otherwise, to get the full directory path joining the parts up to the second-to-last
+	// part with the directory separator that's present in the original file path
+	const separatorUsed = path.includes('\\') ? '\\' : '/';
+	const directoryName = parts.slice(0, parts.length - 1).join(separatorUsed);
+	return ok(directoryName);
+}
+
+/**
+ * Extracts the filename from a given file path.
+ *
+ * @param path - The file path from which to extract the filename.
+ * @returns A `Result` object containing the filename as a `string` on success,
+ * or an `Error` object if the provided input is not a string.
+ *
+ * @example
+ * ```
+ * filename('/home/demo/pictures/dummy.jpeg').value
+ * // => "dummy.jpeg"
+ *
+ * filename('/home/demo/profile').value
+ * // => "profile"
+ *
+ * filename('/home/demo/pictures/').value
+ * // => ""
+ *
+ * filename('C:\\Users\\Documents\\File1.txt').value
+ * // => 'File1.txt'
+ *
+ * filename('/var/www/html/index.html').value
+ * // => 'index.html''
+ *
+ * filename('https://example.com/assets/image.png').value
+ * // => 'image.png'
+ *
+ * const fname = filename(12345)
+ * fname.isOk()
+ * // => false
+ * fname.isErr()
+ * // true
+ * fname.error.message
+ * // => '[paths.filename] Expected string value as input'
+ * ```
+ */
+export function filename(path: string): Result<string, Error> {
+	if (!isString(path)) {
+		return err(new Error('[paths.filename] Expected string value as input'));
+	}
+
+	return ok(isFile(path) ? path.replace(/^.*[\\\\/]/, '') : '');
+}
+
+/**
+ * Checks if a given string represents an image file path with extensions such
  * as jpg, jpeg, png, gif, webp, or avif.
  *
- * @param {string} str - a string that represents a file name or URL.
+ * @param str - The string to be checked.
+ * @returns `true` if the given string represents an image file path, `false` otherwise.
  *
- * @returns a boolean value. It returns `true` if the input string matches the regular expression
- * pattern for an image file (ending in .jpg, .jpeg, .png, .gif, .webp, or .avif), and `false`
- * otherwise.
+ * @example
+ * ```
+ * isImage('dummy.jpeg')
+ * // => true
+ *
+ * isImage('dummy.weBp')
+ * // => true
+ *
+ * isImage('dummy.PNG')
+ *
+ * // => true
+ *
+ * isImage('dummy.docx')
+ * // => false
+ *
+ * isImage('/home/demo/dummy')
+ * // => false
+ *
+ * isImage(undefined)
+ * // => false
+ * ```
  */
-export function isImage(str: string): boolean {
-	if (!filename(str)) return false;
-	const regex = new RegExp(/[^\s]+(.*?).(jpg|jpeg|png|gif|webp|avif)$/i);
+export function isImage(path: string): boolean {
+	if (!isDefined(path) || !isString(path) || !isFile(path)) return false;
 
-	if (str == null || typeof str === 'undefined') return false;
+	const imageRegex = new RegExp(/[^\s]+(.*?).(jpg|jpeg|png|gif|webp|avif)$/i);
 
-	if (regex.test(str) == true) return true;
-
-	return false;
+	return imageRegex.test(path);
 }
 
 /**
- * The function returns the last segment of a given path string.
+ * Returns the last segment of a given path string.
  *
- * @param {string} thePath - The input string representing a file path or URL.
+ * @param path - The path string.
+ * @returns A `Result` with the last segment as a string on success, and an `Error` object if the
+ * provided input is not a string.
  *
- * @returns  the last segment of a given path string.
+ * @example
+ * ```
+ * lastSegment('/home/user/documents/file.txt').value
+ * // => file.txt
+ *
+ * lastSegment('/home/user/documents/').value
+ * // => ''
+ *
+ * lastSegment('/home/user/documents').value
+ * //  => 'documents'
+ *
+ * lastSegment('file.txt').value
+ * //  => 'file.txt'
+ *
+ * lastSegment('').value
+ * // => ''
+ *
+ * const seg1 = lastSegment(null)
+ * seg1.isOk()
+ * // => false
+ * seg1.isErr()
+ * // => true
+ * seg1.error.message
+ * // => [paths.lastSegment] Expected string value as input
+ *
+ * const seg2 = lastSegment(12345)
+ * seg2.isOk()
+ * // => false
+ * seg2.isErr()
+ * // => true
+ * seg2.error.message
+ * // => Error: [paths.lastSegment] Expected string value as input
+ * ```
  */
-export function lastSegment(thePath: string): string {
-	return thePath.substring(thePath.lastIndexOf('/') + 1);
+export function lastSegment(path: string): Result<string, Error> {
+	if (!isString(path)) {
+		return err(new Error('[paths.lastSegment] Expected string value as input'));
+	}
+
+	// Unix root path
+	const unixRegex = /^\/$/;
+	// Windows drive letter path (with or without escaping)
+	const windowsRegex = /^([a-z]:)\\$/i;
+
+	if (unixRegex.test(path) || windowsRegex.test(path)) {
+		return ok('');
+	}
+
+	const separator = /[\\/]/; // Match both Windows and Unix separators
+	const segments = path.split(separator); // Split the path by separator
+	const lastSegment = segments[segments.length - 1]; // Get the last segment
+
+	// If the last segment is an empty string, it means the path ended with a separator
+	// In that case, we get the second-to-last segment as the last segment
+	if (isEmpty(lastSegment)) {
+		return ok(segments[segments.length - 2] || '');
+	}
+
+	return ok(lastSegment);
 }
--- tests/paths.test.ts
+++ tests/paths.test.ts
@@ -1,15 +1,111 @@
 import { describe, it, expect } from 'vitest';
-import { lastSegment, dirname, filename, isImage } from '../src/paths';
+import { isDir, isFile, lastSegment, dirname, filename, isImage } from '../src/paths/index.js';
+import { ok } from 'neverthrow';
+
+describe('isDir', () => {
+	it('should be true', () => {
+		expect(isDir('/home/demo/pictures')).toBe(true);
+		expect(isDir('/home/demo/pictures/')).toBe(true);
+		expect(isDir('C:\\Users\\Documents')).toBe(true);
+	});
+
+	it('should be false', () => {
+		expect(isDir('/home/demo/pictures/dummy.jpeg')).toBe(false);
+		expect(isDir('C:\\Users\\Documents\\File1.txt')).toBe(false);
+	});
+
+	it('should be false for non string input', () => {
+		expect(isDir(10)).toBe(false);
+	});
+});
+
+describe('isFile', () => {
+	it('should be true', () => {
+		expect(isFile('/home/demo/pictures/dummy.jpeg')).toBe(true);
+		expect(isFile('/home/demo/profile')).toBe(true);
+		expect(isFile('C:\\Users\\Documents\\File1.txt')).toBe(true);
+	});
+
+	it('should be false', () => {
+		expect(isFile('/home/demo/pictures/')).toBe(false);
+	});
+
+	it('should be false for non string input', () => {
+		expect(isFile(10)).toBe(false);
+	});
+});
 
 describe('dirname', () => {
 	it('should be pictures', () => {
-		expect(dirname('/home/demo/pictures/dummy.jpeg')).toBe('/home/demo/pictures');
+		const result = dirname('/home/demo/pictures/dummy.jpeg');
+		expect(result).toEqual(ok('/home/demo/pictures'));
+	});
+
+	it('should be C:\\Users\\Documents', () => {
+		expect(dirname('C:\\Users\\Documents\\File1.txt')).toEqual(ok('C:\\Users\\Documents'));
+	});
+
+	it('should be /home/demo', () => {
+		const result = dirname('/home/demo/pictures');
+		expect(result).toEqual(ok('/home/demo'));
+	});
+
+	it('should be file.txt, the given path is just a file name', () => {
+		const result = dirname('file.txt');
+		expect(result).toEqual(ok('file.txt'));
+	});
+
+	it('should be C:', () => {
+		const result = dirname('C:\\');
+		expect(result).toEqual(ok('C:'));
+	});
+
+	it('should be https://example.com/assets/', () => {
+		expect(dirname('https://example.com/assets/image.png')).toEqual(
+			ok('https://example.com/assets')
+		);
+	});
+
+	it('should be an error with "[paths.dirname] The given path must be a string"', () => {
+		const result = dirname(10);
+
+		expect(result.isErr()).toBe(true);
+		expect(result._unsafeUnwrapErr().message).toBe(
+			'[paths.dirname] The given path must be a string'
+		);
+		expect(result._unsafeUnwrapErr()).toStrictEqual(
+			new Error('[paths.dirname] The given path must be a string')
+		);
 	});
 });
 
 describe('filename', () => {
 	it('should be dummy.jpeg', () => {
-		expect(filename('/home/demo/pictures/dummy.jpeg')).toBe('dummy.jpeg');
+		expect(filename('/home/demo/pictures/dummy.jpeg')).toEqual(ok('dummy.jpeg'));
+	});
+
+	it('should be File1.txt', () => {
+		expect(filename('C:\\Users\\Documents\\File1.txt')).toEqual(ok('File1.txt'));
+	});
+
+	it('should be image.png', () => {
+		expect(filename('https://example.com/assets/image.png')).toEqual(ok('image.png'));
+	});
+
+	it('should be the same as the given input', () => {
+		expect(filename('/home/demo/pictures/')).toEqual(ok(''));
+	});
+
+	it('should be an error with "[paths.filename] Expected string value as input"', () => {
+		const result = filename(10);
+
+		expect(result.isErr()).toBe(true);
+		expect(result._unsafeUnwrapErr().message).toBe(
+			'[paths.filename] Expected string value as input'
+		);
+		expect(result._unsafeUnwrapErr()).toStrictEqual(
+			new Error('[paths.filename] Expected string value as input')
+		);
 	});
 });
 
@@ -33,10 +129,38 @@ describe('isImage', () => {
 	it('should be false when empty string', () => {
 		expect(isImage('')).toBe(false);
 	});
+
+	it('should be false for not defined input', () => {
+		expect(isImage(undefined)).toBe(false);
+	});
 });
 
 describe('lastSegment', () => {
 	it('should be pictures', () => {
-		expect(lastSegment('/home/demo/pictures')).toBe('pictures');
+		expect(lastSegment('/home/demo/pictures/')).toEqual(ok('pictures'));
+		expect(lastSegment('C:\\Users\\John\\Documents\\pictures')).toEqual(ok('pictures'));
+	});
+
+	it('should be dummy.jpg', () => {
+		expect(lastSegment('/home/demo/pictures/dummy.jpg')).toEqual(ok('dummy.jpg'));
+		expect(lastSegment('C:\\Users\\John\\Documents\\dummy.jpg')).toEqual(ok('dummy.jpg'));
+	});
+
+	it('should be empty strings', () => {
+		expect(lastSegment('C:\\')).toEqual(ok(''));
+		expect(lastSegment('C:\\\\')).toEqual(ok(''));
+		expect(lastSegment('/')).toEqual(ok(''));
+	});
+
+	it('should be an error with "[paths.lastSegment] Expected string value as input"', () => {
+		const result = lastSegment(10);
+
+		expect(result.isErr()).toBe(true);
+		expect(result._unsafeUnwrapErr().message).toBe(
+			'[paths.lastSegment] Expected string value as input'
+		);
+		expect(result._unsafeUnwrapErr()).toStrictEqual(
+			new Error('[paths.lastSegment] Expected string value as input')
+		);
 	});
 });
