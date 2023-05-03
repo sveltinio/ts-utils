7e05ee7ddab4dba49fb988d444875d19a4fdbeb7
Mirco Veltri <github@mircoveltri.me>
2023-05-03 20:58:48 +0200

    docs(dates): update TSDoc

--- src/dates/index.ts
+++ src/dates/index.ts
@@ -1,91 +1,162 @@
 /**
- * Given a number, return a string that is the number padded to two digits.
+ * A bunch of utilities to deal with dates.
+ *
+ * @packageDocumentation
+ */
+
+import { Ok, Result, err, ok } from 'neverthrow';
+import { isNumber, isString, isDate } from '../is';
+
+/**
+ * Pads a number to exactly 2 digits with leading zeros.
  * Used if the month and day values only contain a single digit (are less than 10).
  *
- * @param num - number - The number to pad.
+ * @param num - The number to pad.
+ * @returns A `Result` object with either the padded number as a string on success, or an `Error`
+ * object if the provided input is not a number.
  *
- * @returns A string with the number padded to two digits.
- *
- * @example
- * ```
- * // Prints 08:
- * padTo2Digits(8);
- * ```
- */
-export function padTo2Digits(num: number): string {
-	return num.toString().padStart(2, '0');
-}
-
-/**
- * It takes a date object and returns a string in the format DD/MM/YYYY
- *
- * @param date - Date - The date to format.
- *
- * @returns A string in the format DD/MM/YYYY.
- *
- * @example
- * ```
- * // Prints "07/03/2023":
- * formatDate(new Date('2023-03-07T03:24:00'));
- * ```
- */
-export function formatDate(date: Date): string {
-	return [padTo2Digits(date.getDate()), padTo2Digits(date.getMonth() + 1), date.getFullYear()].join(
-		'/'
-	);
-}
-
-/**
- * The function formats a given date object into an ISO string format.
- *
- * @param {Date} date - The input parameter of type Date that represents the date to be formatted.
- *
- * @returns A string representing the date in ISO format (YYYY-MM-DD).
  *
  *  @example
+ * ```typescript
+ * padTo2Digits(7)
+ * // => { ok: true, value: '07' }
  * ```
- * // Prints "2023-04-17":
- * formatDate(new Date(2023, 3, 17));
+ *
+ * @example
+ * Handle invalid input:
+ * ```typescript
+ * padTo2Digits("hello")
+ * // => { ok: false, error: Error('[dates.padTo2Digits] Expected number value as input') }
  * ```
  */
-export function formatDateISO(date: Date): string {
-	return [date.getFullYear(), padTo2Digits(date.getMonth() + 1), padTo2Digits(date.getDate())].join(
-		'-'
-	);
+export function padTo2Digits(num: number): Result<string, Error> {
+	if (!isNumber(num)) {
+		return err(new Error('[dates.padTo2Digits] Expected number value as input'));
+	}
+
+	return ok(num.toString().padStart(2, '0'));
 }
 
 /**
- * Given a date string in the format MM/DD/YYYY, return the day of the month.
+ * Formats a Date object into a string representation of the date in the format "dd/mm/yyyy".
  *
- * @param dateStr - string - The date string in the format MM/DD/YYYY to be parsed.
+ * @param date - The date to format.
+ * @returns A `Result` object with either a string containing the formatted date
+ * in the format "dd/mm/yyyy" on success, or an `Error` object if the input value is not
+ * a valid `Date` object.
  *
- * @returns The day of the month from the date string.
+ *  @example
+ * ```typescript
+ * formatDate(new Date('2023-03-07T03:24:00'))
+ * // => { ok: true, value: '07/03/2023' }
+ * ```
  *
  * @example
- * ```
- * // Prints 3:
- * dayOfMonthFromDate("07/03/2023");
+ * Handle invalid input:
+ * ```typescript
+ * formatDate(10)
+ * // => { ok: false, error: Error('[dates.formatDate] Expected string value as input') }
  * ```
  */
-export function dayOfMonthFromDate(dateStr: string): number {
-	const dt = new Date(dateStr);
-	return dt.getDate();
+export function formatDate(date: Date): Result<string, Error> {
+	if (!isDate(date)) {
+		return err(new Error('[dates.formatDate] Expected date value as input'));
+	}
+
+	const day = padTo2Digits(date.getDate()) as Ok<string, never>;
+	const month = padTo2Digits(date.getMonth() + 1) as Ok<string, never>;
+	const year = date.getFullYear();
+
+	return ok([day.value, month.value, year].join('/'));
 }
 
 /**
- * Given a date string in the format MM/DD/YYYY, return the short month name.
+ * Formats a Date object into a string representation of the date in the ISO 8601 format "yyyy-mm-dd".
  *
- * @param dateStr - string - The date string in the format MM/DD/YYYY to convert to a month short.
+ * @param date - The Date object to format.
+ * @returns  A `Result` object with a string containing the formatted date
+ * in the ISO 8601 format "yyyy-mm-dd" on success, or an Error object if the provided
+ * input is not a valid Date object.
  *
- * @returns A string representing the month of the date passed in.
+ *  @example
+ * ```typescript
+ * formatDateISO(new Date())
+ * // => { ok: true, value: '2023-04-25' }
+ * ```
  *
  * @example
- * ```
- * // Prints Jul:
- * monthShortFromDate("07/03/2023");
+ * Handle invalid input:
+ * ```typescript
+ * formatDateISO(10)
+ * // => { ok: false, error: Error('[dates.formatDateISO] Expected string value as input') }
  * ```
  */
-export function monthShortFromDate(dateStr: string): string {
+export function formatDateISO(date: Date): Result<string, Error> {
+	if (!isDate(date)) {
+		return err(new Error('[dates.formatDateISO] Expected date value as input'));
+	}
+
+	const year = date.getFullYear();
+	const month = padTo2Digits(date.getMonth() + 1) as Ok<string, never>;
+	const day = padTo2Digits(date.getDate()) as Ok<string, never>;
+
+	return ok([year, month.value, day.value].join('-'));
+}
+
+/**
+ * Extracts the day of the month as a number from a given date string.
+ *
+ * @param dateStr - The date string to extract the day of the month from.
+ * @returns A `Result` object with either the day of the month as a number on success,
+ * or an `Error` object if the input value is not a valid date string.
+ *
+ *  @example
+ * ```typescript
+ * dayOfMonth("07/03/2023")
+ * // => { ok: true, value: '3' }
+ * ```
+ *
+ * @example
+ * Handle invalid input:
+ * ```typescript
+ * dayOfMonth(10)
+ * // => { ok: false, error: Error('[dates.dayOfMonth] Expected string value as input') }
+ * ```
+ */
+export function dayOfMonth(dateStr: string): Result<number, Error> {
+	if (!isString(dateStr)) {
+		return err(new Error('[dates.dayOfMonth] Expected string value as input'));
+	}
+
 	const dt = new Date(dateStr);
-	return dt.toLocaleString('default', { month: 'short' });
+	return ok(dt.getDate());
+}
+
+/**
+ * Extracts the short name of the month from a given date string.
+ *
+ * @param dateStr - The date string to extract the short name of the month from.
+ * @returns A `Result` object with either the short name of the month on success,
+ * or an `Error` object if the input value is not a valid date string.
+ *
+ *  @example
+ * ```typescript
+ * monthShort("07/03/2023")
+ * // => { ok: true, value: 'Jul' }
+ * ```
+ *
+ * @example
+ * Handle invalid input:
+ * ```typescript
+ * monthShort(10)
+ * // => { ok: false, error: Error('[dates.monthShort] Expected string value as input') }
+ * ```
+ */
+export function monthShort(dateStr: string): Result<string, Error> {
+	if (!isString(dateStr)) {
+		return err(new Error('[dates.monthShort] Expected string value as input'));
+	}
+
+	const dt = new Date(dateStr);
+	return ok(dt.toLocaleString('default', { month: 'short' }));
 }
--- tests/dates.test.ts
+++ tests/dates.test.ts
@@ -1,66 +1,127 @@
 import { describe, it, expect } from 'vitest';
 import {
 	padTo2Digits,
-	dayOfMonthFromDate,
-	monthShortFromDate,
+	dayOfMonth,
+	monthShort,
 	formatDate,
 	formatDateISO
-} from '../src/dates';
+} from '../src/dates/index.js';
+import { ok } from 'neverthrow';
 
 describe('padTo2Digits', () => {
 	it('should be 08 as string', () => {
-		expect(padTo2Digits(8)).toBe('08');
+		expect(padTo2Digits(8)).toEqual(ok('08'));
 	});
 
 	it('should be 28 as string', () => {
-		expect(padTo2Digits(28)).toBe('28');
+		expect(padTo2Digits(28)).toEqual(ok('28'));
+	});
+
+	it('should be an error with "[dates.padTo2Digits] Expected number value as input"', () => {
+		const result = padTo2Digits('');
+
+		expect(result.isErr()).toBe(true);
+		expect(result._unsafeUnwrapErr().message).toBe(
+			'[dates.padTo2Digits] Expected number value as input'
+		);
+		expect(result._unsafeUnwrapErr()).toStrictEqual(
+			new Error('[dates.padTo2Digits] Expected number value as input')
+		);
 	});
 });
 
-describe('dayOfMonthFromDate', () => {
+describe('dayOfMonth', () => {
 	it('should be 12', () => {
 		const date = '11/12/2023';
-		expect(dayOfMonthFromDate(date)).toBe(12);
+		expect(dayOfMonth(date)).toEqual(ok(12));
 	});
 
 	it('should be NaN', () => {
 		const date = '20/12/2023';
-		expect(dayOfMonthFromDate(date)).toBeNaN();
+		expect(dayOfMonth(date)).toEqual(ok(NaN));
+	});
+
+	it('should be an error with "[dates.dayOfMonth] Expected string value as input"', () => {
+		const result = dayOfMonth(10);
+
+		expect(result.isErr()).toBe(true);
+		expect(result._unsafeUnwrapErr().message).toBe(
+			'[dates.dayOfMonth] Expected string value as input'
+		);
+		expect(result._unsafeUnwrapErr()).toStrictEqual(
+			new Error('[dates.dayOfMonth] Expected string value as input')
+		);
 	});
 });
 
-describe('monthShortFromDate', () => {
+describe('monthShort', () => {
 	it('should be Nov', () => {
 		const date = '11/12/2023';
-		expect(monthShortFromDate(date)).toBe('Nov');
+		expect(monthShort(date)).toEqual(ok('Nov'));
 	});
 
 	it('should be Mar', () => {
 		const date = '03/12/2023';
-		expect(monthShortFromDate(date)).toBe('Mar');
+		expect(monthShort(date)).toEqual(ok('Mar'));
+	});
+
+	it('should be an error with "[dates.monthShort] Expected string value as input"', () => {
+		const result = monthShort(10);
+
+		expect(result.isErr()).toBe(true);
+		expect(result._unsafeUnwrapErr().message).toBe(
+			'[dates.monthShort] Expected string value as input'
+		);
+		expect(result._unsafeUnwrapErr()).toStrictEqual(
+			new Error('[dates.monthShort] Expected string value as input')
+		);
 	});
 });
 
 describe('formatDate', () => {
 	it('should be 07/03/2023', () => {
 		const date = new Date('2023-03-07T03:24:00');
-		expect(formatDate(date)).toBe('07/03/2023');
+		expect(formatDate(date)).toEqual(ok('07/03/2023'));
 	});
 
 	it('should be 01/01/2023', () => {
 		const date = new Date(2023, 0, 1);
-		expect(formatDate(date)).toBe('01/01/2023');
+		expect(formatDate(date)).toEqual(ok('01/01/2023'));
+	});
+
+	it('should be an error with "[dates.formatDate] Expected date value as input"', () => {
+		const result = formatDate(10);
+
+		expect(result.isErr()).toBe(true);
+		expect(result._unsafeUnwrapErr().message).toBe(
+			'[dates.formatDate] Expected date value as input'
+		);
+		expect(result._unsafeUnwrapErr()).toStrictEqual(
+			new Error('[dates.formatDate] Expected date value as input')
+		);
 	});
 });
 
 describe('formatDateISO', () => {
 	it('should be 2023-04-17', () => {
 		const date = new Date('2023-04-17T03:24:00');
-		expect(formatDateISO(date)).toBe('2023-04-17');
+		expect(formatDateISO(date)).toEqual(ok('2023-04-17'));
 	});
 
 	it('should be 2023-04-17', () => {
 		const date = new Date(2023, 3, 17);
-		expect(formatDateISO(date)).toBe('2023-04-17');
+		expect(formatDateISO(date)).toEqual(ok('2023-04-17'));
+	});
+
+	it('should be an error with "[dates.formatDateISO] Expected date value as input"', () => {
+		const result = formatDateISO(10);
+
+		expect(result.isErr()).toBe(true);
+		expect(result._unsafeUnwrapErr().message).toBe(
+			'[dates.formatDateISO] Expected date value as input'
+		);
+		expect(result._unsafeUnwrapErr()).toStrictEqual(
+			new Error('[dates.formatDateISO] Expected date value as input')
+		);
 	});
 });
